d39f3abe6522f6fecb6c7c8fc1dd1f16795e5192
Alex Chan <alexchantastic@gmail.com>
2018-07-13 21:22:11 -0700

    Use webpack for build

--- src/app.coffee
+++ src/app.coffee
@@ -5,3 +5,12 @@
 
 # Add your Framer code here
 # Refer to https://framer.com/docs/ for documentation
+
+layerA = new Layer
+    x: 100
+    y: 100
+    width: 250
+    height: 250
+    opacity: 0.5
+    backgroundColor: "white"
+
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+const CleanWebpackPlugin = require('clean-webpack-plugin'),
+      CopyWebpackPlugin = require('copy-webpack-plugin'),
+      HtmlWebpackPlugin = require('html-webpack-plugin'),
+      webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/app.coffee'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.coffee$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ['coffee-loader']
+      },
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['./dist']),
+    new CopyWebpackPlugin([
+      {
+        from: './src/framer',
+        to: 'framer'
+      }
+    ]),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  devServer: {
+    port: 3000,
+    contentBase: path.resolve(__dirname, 'dist')
+  }
+};
