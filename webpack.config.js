5c6c9cb9c51d5680395a969ea4dcaf17d70f1624
Cadin Batrack <cabatrac@microsoft.com>
2023-05-20 13:11:30 -0700

    update project config to resolve build errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,56 @@
-const path = require('path');
+const path = require("path")
 
-const CleanWebpackPlugin = require('clean-webpack-plugin'),
-      CopyWebpackPlugin = require('copy-webpack-plugin'),
-      HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require("clean-webpack-plugin"),
+    CopyWebpackPlugin = require("copy-webpack-plugin"),
+    HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    app: './src/app.coffee'
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.coffee$/,
-        exclude: /(node_modules|bower_components)/,
-        use: ['coffee-loader']
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(['./dist']),
-    new CopyWebpackPlugin([
-      {
-        from: './src/framer',
-        to: 'framer'
-      },
-      {
-        from: './src/images',
-        to: 'images'
-      },
-      {
-        from: './src/imported',
-        to: 'imported'
-      }
-    ]),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      favicon: './src/favicon.png'
-    })
-  ],
-  resolve: {
-    extensions: ['.wasm', '.mjs', '.js', '.json', '.coffee'],
-    modules: [
-      path.resolve(__dirname, 'src/modules'),
-      'node_modules'
-    ]
-  },
-  devServer: {
-    port: 3000,
-    contentBase: path.resolve(__dirname, 'dist')
-  }
-};
+    mode: "development",
+    entry: {
+        app: "./src/app.coffee",
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.coffee$/,
+                exclude: /(node_modules|bower_components)/,
+                use: ["coffee-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(["./dist"]),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: "./src/framer",
+                    to: "framer",
+                },
+                {
+                    from: "./src/images",
+                    to: "images",
+                },
+                {
+                    from: "./src/imported",
+                    to: "imported",
+                },
+            ],
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            favicon: "./src/favicon.png",
+        }),
+    ],
+    resolve: {
+        extensions: [".wasm", ".mjs", ".js", ".json", ".coffee"],
+        modules: [path.resolve(__dirname, "src/modules"), "node_modules"],
+    },
+    devServer: {
+        port: 3000,
+        static: path.resolve(__dirname, "dist"),
+    },
+}
